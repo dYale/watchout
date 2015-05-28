3f75c28d13bfc1d37478a2cf066dffb40b95de2f
Dan Corman <dan.corman@gmail.com>
2015-05-28 15:42:47 -0500

    Merge conflict resolved

---
+++ PlayerConstructor.js
@@ -0,0 +1,55 @@
+// var Player = function(id){
+// 	d3.select('.container').select('svg')
+// 	.append("svg:circle")
+// 	.attr("cx", 15)
+// 	.attr("cy", 15)
+// 	.attr("r", 8)
+// 	this.id = id;
+// 	this.x = 0;
+// 	this.y = 0;
+// }
+
+
+
+// var playerArray = [];
+
+
+// var addPlayers = function(n){
+// 	for(var i = 0; i < n; i++){
+
+// 		var playa = new Player(i);
+// 		playerArray.push(playa);
+// 	}
+// }
+
+
+// Player.prototype.getX = function(){
+// 	return this.x;
+// };
+
+// Player.prototype.setX = function(x){
+// 	var max;
+// 	var min;
+// 	max = this.gameOptions.padding;
+// 	min = this.gameOptions.height - this.gameOptions.padding;
+// 	if (x <= min) x = min;
+// 	if (x >= max) x = max;	
+// }
+
+// Player.prototype.getY = function(){
+// 	return this.y;
+// };
+
+// Player.prototype.setY = function(y){
+// 	var max;
+// 	var min;
+// 	max = this.gameOptions.padding;
+// 	min = this.gameOptions.height - this.gameOptions.padding;
+// 	if (y <= min) y = min;
+// 	if (y >= max) y = max;	
+// }
+
+
+
+
+
